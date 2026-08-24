7c3209b845a29043b56c3a17075da2c2a3423217
Daninkk <flamezueiro@gmail.com>
2026-08-24 16:24:33 -0300

    refactor: troquei a abordagem para um POO

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,35 +1,36 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise ValueError("A variável de ambiente 'DATABASE_URL' não foi encontrada. Verifique o seu arquivo .env.")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+class Database:
+    def __init__(self, database_url: str):
+        
+        if not database_url:
+            raise ValueError("DATABASE_URL não encontrada.")
+        
+        self.engine = create_engine(database_url)
+        self.SessionLocal = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=self.engine
+        )
+        self.Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    def get_db(self):
+        db = self.SessionLocal()
 
-def test_connection():
-    """Testa a conexão com o PostgreSQL."""
-    try:
-        with engine.connect() as connection:
-            result = connection.execute(text("SELECT current_database();"))
-            db_name = result.scalar()
-            print(f"[OK] Conexao bem-sucedida com o banco de dados PostgreSQL: '{db_name}'!")
-            return True
-    except Exception as e:
-        print(f"[ERRO] Falha ao conectar ao banco de dados: {e}")
-        return False
+        try:
+            yield db
+        finally:
+            db.close()
 
-if __name__ == "__main__":
-    test_connection()
+    def test_connection(self):
+        try:
+            with self.engine.connect() as connection:
+                result = connection.execute(text("SELECT current_database();"))
+                db_name = result.scalar()
+                print(f"[OK] Conectado ao banco '{db_name}'")
+                return True
+        except Exception as e:
+            print(f"[ERRO] {e}")
+            return False
