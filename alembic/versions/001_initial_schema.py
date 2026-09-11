8021e6ad63f82521d6110efdea654b6b61d9abb7
Daninkk <flamezueiro@gmail.com>
2026-09-10 22:31:37 -0300

    fix: adição de migrations no ci

--- alembic/versions/001_initial_schema.py
+++ alembic/versions/001_initial_schema.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '001_initial_schema'
-down_revision: str | Sequence[str] | None = None
+down_revision: str | Sequence[str] | None = "27a339618b8f"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
